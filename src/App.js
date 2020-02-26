9aa162f72bf40ee7bcb4ee86ec5d8e09c25cc3a1
dannielarenderos <00087717@uca.edu.sv>
2020-02-26 15:47:48 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './bootstrap.min.css'; 
+import './App.css';
+import {Switch, Route} from "react-router-dom";
+
+import Register from './components/Register';
+import Login from './components/Login';
+import Splash from './components/Splash';
+import ReactFeed from './components/Feed/ReactFeed'
+
+
+function App() {
+  return (
+  <Switch>
+
+    <Route path = "/register" component = {Register} />
+    <Route path = "/login" component = {Login} />
+    <Route path = "/"> 
+      <Splash>
+        <ReactFeed/>
+      </Splash>
+    ></Route>
+
+
+  </Switch>
+
+);
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {BrowserRouter as Router} from "react-router-dom";
+
+const RouterApp = (<Router> <App/> </Router>);
+
+
+ReactDOM.render(RouterApp, document.getElementById('root'));
+
